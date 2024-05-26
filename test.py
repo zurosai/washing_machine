3fd54b05337b8a3f3ff5f5b059074bcbaa51c1dc
zurosai <isaizurita@gmail.com>
2024-05-25 23:30:01 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+from machine import UART, Pin, I2C
+from ssd1306 import SSD1306_I2C
+
+# Configuración del bus 12c
+i2c = I2C(id=0, scl=Pin(17), sda=Pin(16), freq=400000)
+
+# Configuración OLED
+oled_width = 128
+oled_height = 64
+oled = SSD1306_I2C(oled_width, oled_height, i2c)
+
+# Configuración UART
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+while True:
+    oled.fill(0)
+    oled.text(data, 0, 0)
+    oled.show()
+
+    if uart.any():
+        # Lee datos recibidos por UART
+        data = uart.read()
+        if data:
+            oled.fill(0)
+            oled.text(data, 0, 0)
+            oled.show
+            print("Recibido:", data)
+            # Envía un mensaje de vuelta
+            #uart.write("Hola desde el Pico W (MicroPython)!\n")
